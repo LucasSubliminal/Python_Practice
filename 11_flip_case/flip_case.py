540383117c0f5c697f27f35fdcf71cf7cce82367
Lucas Herrera <lucasherrera@Lucass-MacBook-Air.local>
2024-02-22 21:14:41 -0700

    Project Files Added

---
+++ 11_flip_case/flip_case.py
@@ -0,0 +1,19 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    swapped_char = phrase.swapcase()
+    for char in swapped_char:
+        if char == to_swap:
+            return swapped_char
+            
+
